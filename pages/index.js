d7e294e3a0fcbde67a12dc018b8ca36132d6244e
FieldEz <garfewzaba55@gmail.com>
2023-05-31 13:42:06 +0700

    font

--- pages/index.js
+++ pages/index.js
@@ -10,38 +10,38 @@ export default function index() {
     <Head>
       <title>NExtJs</title>
     </Head>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Drugs Dealler</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">บ้าน</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">เชื่อม</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            ล่วงลง
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">การกระทำ</a></li>
+            <li><a class="dropdown-item" href="#">การกระทำอีกอัน</a></li>
             <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">อะไรสักอย่างตรงนี้</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link disabled">ปิดไปแล้ว</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-success" type="submit">ค้นหา</button>
       </form>
     </div>
   </div>
@@ -51,7 +51,7 @@ export default function index() {
       <Image src="/hutao.jfif" class="img-thumbnail" alt="hutao" width={200} height={200}/>     
       </div>
 
-    <div className='text-center'><button type="button" class="btn btn-danger" w >HU TAO</button></div>
+    <div className='text-center'><button type="button" class="btn btn-danger" >HU TAO</button></div>
     </>
   )
 }
