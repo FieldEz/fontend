ebcb98370cd511ce8e711d00e8bad7ef1f475c3b
FieldEz <garfewzaba55@gmail.com>
2023-06-07 11:00:00 +0700

    ไ

--- pages/about.js
+++ pages/about.js
@@ -57,11 +57,11 @@ export default function about() {
 
 <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval={300}>
+    <div className="carousel-item active" data-bs-interval={3000}>
       <img src="/ab1.jpg" className="d-block w-100" alt="gtr1" />
     </div>
-    <div className="carousel-item" data-bs-interval={300}>
-      <img src="/ab2.jpeg" className="d-block w-100" alt="gtr2" />
+    <div className="carousel-item" data-bs-interval={3000}>
+      <img src="/ab2.jpg" className="d-block w-100" alt="gtr2" />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
