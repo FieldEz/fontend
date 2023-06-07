700ba1c944fa4ab1bc6ea40125a6bccbe4217261
FieldEz <garfewzaba55@gmail.com>
2023-06-07 10:15:30 +0700

    w

--- pages/contact.js
+++ pages/contact.js
@@ -13,34 +13,38 @@ export default function contact() {
 </div>
 
     </Head>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark" >
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/.">BOOKFORLIFE</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-danger">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">BOOKFORLIFE</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" href="/.">หน้าหลัก</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" href="/.">หน้าแรก</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" href="about">เกี่ยวกับ</Link>
+        <li className="nav-item">
+          <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            เพิ่มเติม
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            บริการของเรา
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">หน้าร้าน</a></li>
-            <li><a class="dropdown-item" href="contact">ติดต่อ</a></li>
-            <li><hr class="dropdown-divider"/></li>
+          <ul className="dropdown-menu">
+          <li><a className="dropdown-item" href="#">ซื้อออนไลน์</a></li>
+            <li><a className="dropdown-item" href="#">ตรวจสอบหนังสือ</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">สั่งจองหนังสือ</a></li>
           </ul>
         </li>
+        <li className="nav-item">
+          <Link className="nav-link" href="/contact"> ติดต่อเรา</Link>
+        </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">ค้นหา</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-dark" type="submit">ค้นหา</button>
       </form>
     </div>
   </div>
