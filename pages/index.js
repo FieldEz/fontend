cfc0e5accbf013862641bae9c761ac81d2eba370
FieldEz <garfewzaba55@gmail.com>
2023-06-07 10:52:25 +0700

    u

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function index() {
     </Head>
   
   <header>
-<nav className="navbar navbar-expand-lg bg-success">
+<nav className="navbar navbar-expand-lg bg-warning">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">BOOKFORLIFE</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
