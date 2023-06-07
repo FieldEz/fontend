924cf4f361927fcef8e817166e747b8cbb3be213
FieldEz <garfewzaba55@gmail.com>
2023-06-07 10:57:25 +0700

    w

--- pages/about.js
+++ pages/about.js
@@ -49,6 +49,31 @@ export default function about() {
 </nav>
 
 </header>
+
+
+<br />
+<br />
+<br /> 
+
+<div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval={300}>
+      <img src="/ab1.jpg" className="d-block w-100" alt="gtr1" />
+    </div>
+    <div className="carousel-item" data-bs-interval={300}>
+      <img src="/ab2.jpeg" className="d-block w-100" alt="gtr2" />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
 <br />
 <br />
 <br /> 
