48410ed3094816eb16021361219a5dc37bfaabae
FieldEz <garfewzaba55@gmail.com>
2023-06-07 09:35:28 +0700

    w

--- pages/contact.js
+++ pages/contact.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image';
 
 import'bootstrap/dist/css/bootstrap.css';
+import Link from 'next/link';
 
 export default function contact() {
   return (
@@ -23,10 +24,10 @@ export default function contact() {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/.">หน้าหลัก</a>
+          <Link class="nav-link active" aria-current="page" href="/.">หน้าหลัก</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="about">เกี่ยวกับ</a>
+          <Link class="nav-link" href="about">เกี่ยวกับ</Link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
