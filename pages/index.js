1caad70a8355127ae189e171213f9f4b5c1525b4
FieldEz <garfewzaba55@gmail.com>
2023-06-07 09:35:31 +0700

    ww

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,17 @@ export default function index() {
     </Head>
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark" >
     <div class="container-fluid">
-    <a class="navbar-brand" href="/.">BOOKFORLIFE</a>
+    <Link class="navbar-brand" href="/.">BOOKFORLIFE</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/.">หน้าหลัก</a>
+          <Link class="nav-link active" aria-current="page" href="/.">หน้าหลัก</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="about">เกี่ยวกับ</a>
+          <Link class="nav-link" href="about">เกี่ยวกับ</Link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -30,7 +30,7 @@ export default function index() {
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">หน้าร้าน</a></li>
-            <li><a class="dropdown-item" href="contact">ติดต่อ</a></li>
+            <li><Link class="dropdown-item" href="contact">ติดต่อ</Link></li>
             <li><hr class="dropdown-divider"/></li>
           </ul>
         </li>
