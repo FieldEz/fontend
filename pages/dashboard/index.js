8610bd7662d0363bb39980a4dbe909724afcaa5c
FieldEz <garfewzaba55@gmail.com>
2023-06-14 14:28:34 +0700

    qweqwe

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,20 +1,20 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
+    const { data: session } = useSession()
+    if (session) {
+      return (
+        <>
+          Signed in as {session.user.email} <br />
+          {session.user.fname} {session.user.lname} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )
+    }
     return (
       <>
-        Signed in as {session.user.email} <br />
-        {session.user.fname}  {session.user.lname}<br />
-        <button onClick={() => signOut()}>Sign out</button>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
       </>
     )
   }
-  return (
-        <>
-        Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
