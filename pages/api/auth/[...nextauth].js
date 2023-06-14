25371e340a1021665434f8a6f45bd5787a8892d7
FieldEz <garfewzaba55@gmail.com>
2023-06-14 14:19:38 +0700

    we

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -44,12 +44,14 @@ export const authOptions = {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
+        token.user = user
       }
       return token
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
+      token.user = user
       return session
     }
   }
